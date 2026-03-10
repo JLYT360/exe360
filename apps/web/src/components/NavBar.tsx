b00be0c3d965d1b5ce9bf883e3812810311110d4
djewind97 <djewind@hotmail.com>
2026-03-10 09:37:04 -0400

    Simplify NavBar paths - use absolute routes instead of BASE_PATH

--- apps/web/src/components/NavBar.tsx
+++ apps/web/src/components/NavBar.tsx
@@ -1,18 +1,17 @@
 
 import { Link, NavLink } from 'react-router-dom'
-import { BASE_PATH, ASSETS_PATH } from '../config/paths'
 
 export default function NavBar(){
   return (
     <header>
       <div className="nav-container">
-        <Link to={BASE_PATH} className="nav-logo" aria-label="Accueil exe360">
-          <img src={`${ASSETS_PATH}/images/logo.png`} alt="exe360" className="logo-image" />
+        <Link to="/" className="nav-logo" aria-label="Accueil exe360">
+          <img src="/assets/images/logo.png" alt="exe360" className="logo-image" />
           <span>exe360</span>
         </Link>
         <nav className="nav-menu" aria-label="Navigation principale">
-          <NavLink to={`${BASE_PATH}`} className="nav-link">Accueil</NavLink>
-          <NavLink to={`${BASE_PATH}contact`} className="btn btn-nav">Diagnostic express</NavLink>
+          <NavLink to="/" className="nav-link">Accueil</NavLink>
+          <NavLink to="/contact" className="btn btn-nav">Diagnostic express</NavLink>
         </nav>
       </div>
     </header>

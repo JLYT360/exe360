454e99f5bf41123a0cccf73f90cc6b72b40d9a75
djewind97 <djewind@hotmail.com>
2026-03-20 20:04:57 -0400

    Fix NavBar: remove all BASE_PATH references

--- apps/web/src/components/NavBar.tsx
+++ apps/web/src/components/NavBar.tsx
@@ -13,24 +13,8 @@ export default function NavBar(){
           <span>exe360</span>
         </Link>
         <nav className="nav-menu" aria-label="Navigation principale">
-          <NavLink to={`${BASE_PATH}`} className="nav-link">Accueil</NavLink>
-          <div className="nav-dropdown" onMouseEnter={() => setIsServicesOpen(true)} onMouseLeave={() => setIsServicesOpen(false)}>
-            <NavLink to={`${BASE_PATH}services`} className="nav-link" onClick={(e) => e.preventDefault()}>
-              Services ▼
-            </NavLink>
-            {isServicesOpen && (
-              <div className="dropdown-menu">
-                <Link to={`${BASE_PATH}services/marketing-strategique`} className="dropdown-link">Marketing stratégique</Link>
-                <Link to={`${BASE_PATH}services/assistance-administrative`} className="dropdown-link">Assistance administrative</Link>
-                <Link to={`${BASE_PATH}services/precomptabilite`} className="dropdown-link">Pré-comptabilité</Link>
-                <Link to={`${BASE_PATH}services/support-informatique`} className="dropdown-link">Support & Formation</Link>
-              </div>
-            )}
-          </div>
-          <NavLink to={`${BASE_PATH}blog`} className="nav-link">Blog</NavLink>
-          <NavLink to={`${BASE_PATH}a-propos`} className="nav-link">À propos</NavLink>
-          <NavLink to={`${BASE_PATH}aides-financement`} className="nav-link">Aides & Financement</NavLink>
-          <NavLink to={`${BASE_PATH}contact`} className="btn btn-nav">Diagnostic express</NavLink>
+          <NavLink to="/" className="nav-link">Accueil</NavLink>
+          <NavLink to="/contact" className="btn btn-nav">Diagnostic express</NavLink>
         </nav>
       </div>
     </header>
